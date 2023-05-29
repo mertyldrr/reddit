c62b5c2b3aba3fecefbbf182445b89db6731df32
mertyldrr <mertyildirir14@gmail.com>
2023-05-29 17:08:30 +0200

    add noto sans font and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // sagoeUI: "SagoeUI",
+        noto: ["Noto Sans", "sans-serif"],
+      },
+      colors: {
+        redditOrangeRed: "#FF4500",
+        redditUrchin: "#172E35",
+        redditDugong: "#F26F7",
+        redditBeluga: "#FFFFFF",
+        redditDolphin: "#D7DFE2",
+        redditSeal: "#BBC7CC",
+        redditNarwhal: "#7A9299",
+        redditSquidInk: "#000000",
       },
     },
   },
