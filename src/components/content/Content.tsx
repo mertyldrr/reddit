ac492c686b3959c9b1a19293df5a0fc2d495f2bb
mertyldrr <mertyildirir14@gmail.com>
2023-06-03 18:12:51 +0200

    remove h-screen

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,6 +1,6 @@
 export const Content = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-redditNarwhal w-full h-screen pl-[270px] overflow-y-scroll">
+    <div className="flex flex-col justify-center items-center bg-redditNarwhal w-full pl-[270px] px-6 pt-2">
       <div className="text-9xl py-96">Content will appear here...</div>
       <div className="text-9xl py-96">Scrollable on y-axis</div>
     </div>
